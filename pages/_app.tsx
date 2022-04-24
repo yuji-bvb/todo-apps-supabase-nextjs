5bafefa955a02bd9960bf314a836a6fc441c9110
Yuji yanase <yuji.bvb@gmail.com>
2022-04-24 17:37:38 +0900

    change2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,8 @@ export function reportWebVitals(metric:NextWebVitalsMetric){
       console.log(`TTFB:${Math.round(metric.value*10)/10}`)
       break
     case "Next.js-hydration":
-      console.log(`Hydration:${Math.round(metric.value*10)/10}-> ${
-        Math.round((metric.startTime + metric.value*10)/10)
+      console.log(`Hydration:${Math.round(metric.startTime*10)/10}-> ${
+        Math.round((metric.startTime + metric.value)*10)/10
       }`)
       break
     default:
